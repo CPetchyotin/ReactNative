6549c2760a42e6e7e0c6a50279efc0deac22e124
labinstall <labinstall@tnistudent.ac.th>
2023-06-24 11:17:35 +0700

    JavaScrpt

---
+++ javascriptFile/Define_property.js
@@ -0,0 +1,6 @@
+let song  = {};
+song.name = "Panic Attack"
+song.artist =   "Dave"
+song.year = 2021;
+console.log(song
+    )
---
+++ javascriptFile/Output2.js
@@ -0,0 +1,11 @@
+let user = {
+    name : "Chonnanut",
+    age : 19
+};
+function hello(){
+    console.log("Hello , Chonnanut}");
+}
+
+console.log(user);
+console.log([10,20,30]);
+hello();
